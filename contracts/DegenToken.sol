bca5e7eec75f618ff81e84bd53596e1122a54c2f
Mokshith456 <110161392+Mokshith456@users.noreply.github.com>
2023-11-14 21:18:04 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable {
-
-    constructor() ERC20("Degen", "DGN") {}
+contract DegenToken is ERC20, Ownable , ERC20Burnable {
+ string public Degenstore;
+    constructor() ERC20("Degen", "DGN") {
+        Degenstore = "In our official Degen store we have : 1.Official degen Notebook , 2.Official Degen keyboard mat , 3.Official Degen phone cover";
+    }
 
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
     }
+       function redeem(uint256 val) public {
+        require(val>=1 && val<=3);
+        require(balanceOf(msg.sender)>1000, "Insufficient balance");
+            _burn((msg.sender),250);
+       }
 }
